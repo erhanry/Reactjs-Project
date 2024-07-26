03a42a4a012b0e6b03fd9c6b326b1c3065e40fec
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-07-26 13:45:57 +0300

    Create service requester

---
+++ src/services/requester.js
@@ -0,0 +1,39 @@
+const VITE_PROXY_API_TARGET = import.meta.env.VITE_PROXY_API_TARGET || "http://localhost:5000/api";
+
+const requester = async (method, url, data) => {
+    const options = {
+        method,
+        credentials: "include",
+    };
+
+    if (data) {
+        options.headers = {
+            "Content-Type": "application/json",
+        };
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(`${VITE_PROXY_API_TARGET}${url}`, options);
+
+    if (response.status === 204) {
+        return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result;
+};
+
+const request = {
+    get: requester.bind(null, "GET"),
+    post: requester.bind(null, "POST"),
+    put: requester.bind(null, "PUT"),
+    delete: requester.bind(null, "DELETE"),
+    patch: requester.bind(null, "PATCH"),
+};
+
+export default request;
