e719d059c621eed68197995985350a5d579cf46d
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-08-01 19:16:20 +0300

    Add AuthContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,30 @@
-import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { AuthContext } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Products from "./components/products/Products";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import Logout from "./components/logout/Logout";
+import Home from "./components/home/Home";
 
 function App() {
-    const [authState, setAuthState] = useState({});
-
-    const changeAuthState = (state) => {
-        setAuthState(state);
-    };
-
-    const contexData = {
-        firstName: authState.firstName,
-        lastName: authState.lastName,
-        email: authState.email,
-        message: authState.message,
-        isAuthenticated: !!authState.email,
-        cart: [],
-        changeAuthState,
-    };
-
     return (
-        <AuthContext.Provider value={contexData}>
+        <AuthContextProvider>
             <Header />
             <main>
                 <section className="wrapper">
                     <Routes>
-                        <Route path="/" element="" />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/home" element={<Home />} />
                         <Route path="/products" element={<Products />} />
-                        <Route path="/about" element={<p>dfdaadf</p>}>
+                        <Route path="/category/:categoryId" element={<Home />} />
+                        {/* <Route path="/about" element={<p>dfdaadf</p>}>
                             <Route path="mission" element="" />
                             <Route path="our-team" element="" />
                             <Route path="contact-us" element="" />
-                        </Route>
+                        </Route> */}
                         <Route path="/articles" element="" />
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
@@ -51,7 +37,7 @@ function App() {
                 </section>
             </main>
             <Footer />
-        </AuthContext.Provider>
+        </AuthContextProvider>
     );
 }
 
