8424b2a5e41e8ed2a019c87944204fab7881acf2
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-08-11 22:18:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
+import { ErrorBoundary } from "./components/errorBoundary/ErrorBoundary";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ProductContextProvider } from "./context/ProductContext";
+import { AuthGuard, GuestGuard } from "./common/PrivateGuard";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -18,39 +20,45 @@ import Cart from "./components/cart/Cart";
 
 function App() {
     return (
-        <AuthContextProvider>
-            <ProductContextProvider>
-                <Header />
-                <main>
-                    <section className="wrapper">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/products" element={<Products />} />
-                            <Route path="/products/:pageId/page" element={<Products />} />
-                            <Route path="/products/:productId/details" element={<Products />} />
-                            <Route path="/products/:productId/edit" element={<Products />} />
-                            <Route path="/products/:productId/create" element={<Products />} />
+        <ErrorBoundary>
+            <AuthContextProvider>
+                <ProductContextProvider>
+                    <Header />
+                    <main>
+                        <section className="wrapper">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/products" element={<Products />} />
+                                <Route path="/products/:pageId/page" element={<Products />} />
+                                <Route path="/products/:productId/details" element={<Products />} />
+                                <Route path="/products/:productId/edit" element={<Products />} />
+                                <Route path="/products/:productId/create" element={<Products />} />
 
-                            <Route path="/sale" element={<Sale />} />
-                            <Route path="/sale/:pageId/page" element={<Sale />} />
+                                <Route path="/sale" element={<Sale />} />
+                                <Route path="/sale/:pageId/page" element={<Sale />} />
 
-                            <Route path="/category/:categoryPath" element={<Category />} />
+                                <Route path="/category/:categoryPath" element={<Category />} />
 
-                            <Route path="/cart" element={<Cart />} />
-                            <Route path="/about-us" element={<AboutUs />} />
+                                <Route path="/cart" element={<Cart />} />
+                                <Route path="/about-us" element={<AboutUs />} />
 
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/logout" element={<Logout />} />
+                                <Route element={<GuestGuard />}>
+                                    <Route path="/login" element={<Login />} />
+                                    <Route path="/register" element={<Register />} />
+                                </Route>
+                                <Route element={<AuthGuard />}>
+                                    <Route path="/logout" element={<Logout />} />
+                                </Route>
 
-                            <Route path="/not-found" re element={<NotFound />} />
-                            <Route path="/*" element={<Navigate to="/not-found" />} />
-                        </Routes>
-                    </section>
-                </main>
-                <Footer />
-            </ProductContextProvider>
-        </AuthContextProvider>
+                                <Route path="/not-found" re element={<NotFound />} />
+                                <Route path="/*" element={<Navigate to="/not-found" />} />
+                            </Routes>
+                        </section>
+                    </main>
+                    <Footer />
+                </ProductContextProvider>
+            </AuthContextProvider>
+        </ErrorBoundary>
     );
 }
 
